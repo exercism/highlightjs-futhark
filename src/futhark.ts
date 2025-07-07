06801b0eaab88e039543e0e4b58ee6ebad959ba6
Erik Schierboom <erik_schierboom@hotmail.com>
2025-07-07 22:00:19 +0200

    Working on numbers

--- src/futhark.ts
+++ src/futhark.ts
@@ -10,6 +10,7 @@ Website: https://www.futhark.org/
 import { type HLJSApi, type Language, type Mode } from "highlight.js";
 
 export default function (hljs: HLJSApi): Language {
+  const re = hljs.regex;
   const COMMENT: Mode = hljs.COMMENT("--", "$");
 
   const KEYWORDS = [
@@ -39,9 +40,77 @@ export default function (hljs: HLJSApi): Language {
     "with",
   ];
 
+  const DECDIGIT = /[0-9]+/;
+  const HEXDIGIT = /[0-9a-fA-F]+/;
+  const BINDIGIT = /[01]+/;
+
+  const INT_TYPE = /(?:i|u)(?:8|16|32|64)/;
+  const FLOAT_TYPE = /(?:f)(?:16|32|64)/;
+
+  const INTPART = re.concat(
+    DECDIGIT,
+    re.anyNumberOfTimes(re.either(DECDIGIT, "_"))
+  );
+  const FRACTION = re.concat(
+    ".",
+    DECDIGIT,
+    re.anyNumberOfTimes(re.either(DECDIGIT, "_"))
+  );
+  const HEXINTPART = re.concat(
+    HEXDIGIT,
+    re.anyNumberOfTimes(re.either(HEXDIGIT, "_"))
+  );
+  const HEXFRACTION = re.concat(
+    ".",
+    HEXDIGIT,
+    re.anyNumberOfTimes(re.either(HEXDIGIT, "_"))
+  );
+  const EXPONENT = re.concat(/(?:e|E)(?:\+|\-")/, DECDIGIT);
+  const HEXADECIMALFLOAT = re.concat(
+    "0",
+    re.either("x", "X"),
+    HEXINTPART,
+    HEXFRACTION,
+    re.either("p", "P"),
+    re.optional(re.either("+", "-")),
+    DECDIGIT
+  );
+  const EXPONENTFLOAT = re.concat(re.either(INTPART, FRACTION), EXPONENT);
+  const POINTFLOAT = re.concat(re.optional(INTPART), FRACTION);
+  const FLOATNUMBER = re.concat(
+    re.either(POINTFLOAT, EXPONENTFLOAT, HEXADECIMALFLOAT),
+    re.optional(FLOAT_TYPE)
+  );
+
+  const BINARY = re.concat(
+    "0",
+    re.either("b", "B"),
+    BINDIGIT,
+    re.anyNumberOfTimes(re.either(BINDIGIT, "_"))
+  );
+  const HEXADECIMAL = re.concat(
+    "0",
+    re.either("x", "X"),
+    HEXDIGIT,
+    re.anyNumberOfTimes(re.either(HEXDIGIT, "_"))
+  );
+  const DECIMAL = re.concat(
+    DECDIGIT,
+    re.anyNumberOfTimes(re.either(DECDIGIT, "_"))
+  );
+  const INTNUMBER = re.concat(
+    re.either(DECIMAL, HEXADECIMAL, BINARY),
+    re.optional(INT_TYPE)
+  );
+
+  const LITERAL = {
+    className: "literal",
+    begin: re.concat(re.either(INTNUMBER, FLOATNUMBER, "true", "false")),
+  };
+
   return {
     name: "Futhark",
     keywords: KEYWORDS,
-    contains: [COMMENT],
+    contains: [LITERAL, COMMENT],
   };
 }
