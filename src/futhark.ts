5cb7a7ed98bfe6a00695690e363d3e2ec1863f26
Erik Schierboom <erik_schierboom@hotmail.com>
2025-07-07 20:27:33 +0200

    Add comment

--- src/futhark.ts
+++ src/futhark.ts
@@ -10,90 +10,10 @@ Website: https://www.futhark.org/
 import { type HLJSApi, type Language, type Mode } from "highlight.js";
 
 export default function (hljs: HLJSApi): Language {
-  const BUILT_IN: Mode = {
-    scope: "built_in",
-    match:
-      /[◠⌵+⌝’⍤∠˜◡⋯∩□⊓⊸⍩⌈⑄⊛ℂ◇⊟◴∂♭⊙÷⍢↘.∵=⍖⬚⌕⊢¤:⌊∧⊃⋅≥>⊕∘⊗∫⍚⊂▽⊣⧻≤<ₙ⦷≍↥∊∈↧◿×¯¬≠⌅⤚⟜⤸⋕⊜⊡◌ⁿ⚂⇡/⍥☇↯⇌⍏↻⁅≡\\⊏△±∿⍆√?-⨬⊞↙⸮⍉◹⍣⧅°⍜◰⊚◫⤙]/,
-    relevance: 1,
-  };
-
-  const COMMENT: Mode = hljs.HASH_COMMENT_MODE;
-
-  const NUMBER: Mode = {
-    scope: "number",
-    relevance: 0,
-    variants: [
-      { begin: /¯?\d+[eE][-+]?\d+/ }, // exponent notation
-      { begin: /¯?\d+\/¯?\d+/ }, // fraction
-      { begin: /¯?\d+(\.\d+)?/ }, // decimal,
-      { begin: /[ηπτ∞]/ }, // special numbers
-    ],
-  };
-
-  const CHARACTER: Mode = {
-    scope: "string",
-    relevance: 2,
-    variants: [
-      { match: /@\\x[a-zA-Z0-9]{2}/ },
-      { match: /@\\u\{[a-zA-Z0-9]+\}/ },
-      { match: /@\\u[a-zA-Z0-9]{4}/ },
-      { match: /@\\[0snrtbfv\\]/ },
-      { match: /@./ },
-    ],
-  };
-
-  const PLACEHOLDER: Mode = {
-    scope: "subst",
-    match: "_",
-  };
-
-  const STRING: Mode = {
-    scope: "string",
-    variants: [
-      { begin: /^\$\$ /, end: /$/, contains: [PLACEHOLDER] },
-      { begin: /^\$ /, end: /$/ },
-      {
-        begin: /\$"/,
-        end: /"/,
-        contains: [hljs.BACKSLASH_ESCAPE, PLACEHOLDER],
-      },
-      { begin: /"/, end: /"/, contains: [hljs.BACKSLASH_ESCAPE] },
-    ],
-  };
-
-  const IDENTIFIER: Mode = {
-    scope: "title.function.invoke",
-    match: /\b[a-zA-Z]+(__\d+)?/,
-    relevance: 10,
-  };
-
-  const ARRAY: Mode = {
-    scope: "punctuation",
-    match: /[\[\]_]/,
-  };
-
-  const INLINE_FUNCTION: Mode = {
-    scope: "punctuation",
-    match: /[(){}]/,
-  };
-
-  const SIGNATURE: Mode = {
-    scope: "meta",
-    match: /\|\d+(\.\d+)?/,
-  };
+  const COMMENT: Mode = hljs.COMMENT("--", "$");
 
   return {
     name: "Futhark",
-    contains: [
-      SIGNATURE,
-      INLINE_FUNCTION,
-      ARRAY,
-      IDENTIFIER,
-      STRING,
-      CHARACTER,
-      NUMBER,
-      BUILT_IN,
-      COMMENT,
-    ],
+    contains: [COMMENT],
   };
 }
